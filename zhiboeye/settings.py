e146eaaba36f8b22ccebf8a7183061106a447566
fz <jsnjfz@gmail.com>
2017-03-13 22:41:52 +0800

    初始化的时候查所有频道

--- zhiboeye/settings.py
+++ zhiboeye/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-b6%iyzjm-y&0+a!o2nvh9l3w0@w81g9eo@qs5i&bpo5y+)gx5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
